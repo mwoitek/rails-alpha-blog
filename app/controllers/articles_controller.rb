10dc054763849cb711ae3a91af929ce84292e8b5
Marcio Woitek <woitek@usp.br>
2021-07-28 15:17:59 -0300

    Add create action for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,5 +10,10 @@ class ArticlesController < ApplicationController
 
   def new; end
 
-  def create; end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    # redirect_to article_path(@article)
+    redirect_to @article
+  end
 end
